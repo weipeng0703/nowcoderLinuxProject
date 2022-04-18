ae097e3f1d7d845bf0eb249860a1420bca2bbaf7
weipeng0703 <chenweipeng0703@gmail.com>
2022-04-18 20:04:03 +0800

    2022-4-18_线程同步与几种锁

---
+++ ch3/11-读写锁/rwlock.c
@@ -0,0 +1,88 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: weipeng
+ * @Date: 2022-04-18 16:57:49
+ * @LastEditors: weipeng
+ * @LastEditTime: 2022-04-18 20:02:24
+ */
+/*
+    读写锁的类型 pthread_rwlock_t
+    int pthread_rwlock_init(pthread_rwlock_t *restrict rwlock, const pthread_rwlockattr_t *restrict attr);
+    int pthread_rwlock_destroy(pthread_rwlock_t *rwlock);
+    int pthread_rwlock_rdlock(pthread_rwlock_t *rwlock);
+    int pthread_rwlock_tryrdlock(pthread_rwlock_t *rwlock);
+    int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock);
+    int pthread_rwlock_trywrlock(pthread_rwlock_t *rwlock);
+    int pthread_rwlock_unlock(pthread_rwlock_t *rwlock);
+
+    案例：8个线程操作同一个全局变量。
+    3个线程不定时写这个全局变量，5个线程不定时的读这个全局变量
+*/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// 创建一个共享数据
+int num = 1;
+// pthread_mutex_t mutex;       // 使用互斥锁可以解决控制读和写的问题，但是效率不高
+pthread_rwlock_t rwlock;
+
+void * writeNum(void * arg) {
+
+    while(1) {
+        // 加写锁
+        // 如果有多个线程想要读的话。在此处写锁不会阻拦他们去读，而使用互斥锁的话，不管什么线程过来都会被阻塞
+        pthread_rwlock_wrlock(&rwlock);
+        num++;
+        printf("++write, tid : %ld, num : %d\n", pthread_self(), num);
+        pthread_rwlock_unlock(&rwlock);
+        usleep(100);
+    }
+
+    return NULL;
+}
+
+void * readNum(void * arg) {
+
+    while(1) {
+        // 加读锁
+        pthread_rwlock_rdlock(&rwlock);
+        printf("===read, tid : %ld, num : %d\n", pthread_self(), num);
+        pthread_rwlock_unlock(&rwlock);
+        usleep(100);
+    }
+
+    return NULL;
+}
+
+int main() {
+
+   pthread_rwlock_init(&rwlock, NULL);
+
+    // 创建3个写线程，5个读线程
+    pthread_t wtids[3], rtids[5];
+    for(int i = 0; i < 3; i++) {
+        pthread_create(&wtids[i], NULL, writeNum, NULL);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        pthread_create(&rtids[i], NULL, readNum, NULL);
+    }
+
+    // 设置线程分离
+    for(int i = 0; i < 3; i++) {
+       pthread_detach(wtids[i]);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        pthread_detach(rtids[i]);
+    }
+
+    pthread_exit(NULL);
+
+    pthread_rwlock_destroy(&rwlock);
+
+    return 0;
+}
