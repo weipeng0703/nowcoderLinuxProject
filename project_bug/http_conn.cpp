e098c43048e4b105e27b05f751bcc8340a855f68
weipeng0703 <chenweipeng0703@gmail.com>
2022-05-20 08:59:36 +0800

    修改放入PPT

--- project_bug/http_conn.cpp
+++ project_bug/http_conn.cpp
@@ -12,7 +12,7 @@ const char* error_500_title = "Internal Error";
 const char* error_500_form = "There was an unusual problem serving the requested file.\n";
 
 // 网站的根目录
-const char* doc_root = "/root/project_bug/resources";
+const char* doc_root = "/root/nowcoderLinuxProject/project_bug/resources";
 
 int setnonblocking( int fd ) {
     int old_option = fcntl( fd, F_GETFL );
